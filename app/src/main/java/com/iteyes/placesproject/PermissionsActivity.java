844e47fcea6697e26016ef93290d3eb6e339a8d7
Jorge Cougias <jorge.cougias@tcs.com>
2020-01-06 16:48:37 -0300

    First commit app working.

--- app/src/main/java/com/iteyes/placesproject/PermissionsActivity.java
+++ app/src/main/java/com/iteyes/placesproject/PermissionsActivity.java
@@ -15,6 +15,12 @@ import android.widget.Toast;
 import com.android.volley.RequestQueue;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.inlocomedia.android.engagement.InLocoEngagement;
+import com.inlocomedia.android.engagement.InLocoEngagementOptions;
+import com.inlocomedia.android.engagement.request.FirebasePushProvider;
+import com.inlocomedia.android.engagement.request.PushProvider;
+import com.inlocomedia.android.location.InLoco;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -25,11 +31,14 @@ import com.karumi.dexter.listener.single.PermissionListener;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class PermissionsActivity extends AppCompatActivity {
 
-//    private String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.ACTIVITY_RECOGNITION, Manifest.permission.RECEIVE_BOOT_COMPLETED};
+    private static final int MY_ACCESS_FINE_RETURN_VALUE = 128;
+    private static final int MY_ACCESS_COARSE_RETURN_VALUE = 123;
+    //    private String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.ACTIVITY_RECOGNITION, Manifest.permission.RECEIVE_BOOT_COMPLETED};
     private GoogleMap map;
     private Marker marker;
     private Boolean toggle = true;
@@ -41,6 +50,28 @@ public class PermissionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions);
+
+        InLocoEngagementOptions options = InLocoEngagementOptions.getInstance(this);
+
+        // The App ID you obtained in the dashboard
+        options.setApplicationId(getString(R.string.in_loco_app_id));
+
+        // Verbose mode; enables SDK logging, defaults to true.
+        // Remember to set to false in production builds.
+        options.setLogEnabled(true);
+
+        //Initialize the SDK
+        InLocoEngagement.init(this, options);
+        InLocoEngagement.setUserId(this, "Unique_ID");
+
+        String firebaseToken = FirebaseInstanceId.getInstance().getToken();
+
+        if (firebaseToken != null && !firebaseToken.isEmpty()) {
+            final PushProvider pushProvider = new FirebasePushProvider.Builder()
+                    .setFirebaseToken(firebaseToken)
+                    .build();
+            InLocoEngagement.setPushProvider(this, pushProvider);
+        }
 //        getPermissions();
 //        fireBaseConfig();
 
@@ -50,48 +81,69 @@ public class PermissionsActivity extends AppCompatActivity {
             return;
         }
 
-        btnGrant = findViewById(R.id.btn_grant);
+        boolean statusPermission = true;
 
-        btnGrant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Dexter.withActivity(PermissionsActivity.this)
-                        .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-                        .withListener(new PermissionListener() {
-                            @Override
-                            public void onPermissionGranted(PermissionGrantedResponse response) {
-                                startActivity(new Intent(PermissionsActivity.this, MapActivity.class));
-                                finish();
-                            }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-                            @Override
-                            public void onPermissionDenied(PermissionDeniedResponse response) {
-                                if(response.isPermanentlyDenied()){
-                                    AlertDialog.Builder builder = new AlertDialog.Builder(PermissionsActivity.this);
-                                    builder.setTitle("Permission Denied")
-                                            .setMessage("Permission to access device location is permanently denied. Go to settings to allow the permission.")
-                                            .setNegativeButton("Cancel", null)
-                                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                                @Override
-                                                public void onClick(DialogInterface dialog, int which) {
-                                                    Intent intent = new Intent();
-                                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                                    intent.setData(Uri.fromParts("package", getPackageName(), null));
-                                                }
-                                            })
-                                            .show();
-                                } else {
-                                    Toast.makeText(PermissionsActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
-                                }
-                            }
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_ACCESS_FINE_RETURN_VALUE);
 
-                            @Override
-                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-                                token.continuePermissionRequest();
-                            }
-                        })
-                        .check();
-            }
-        });
+        } else {
+            // Permission has already been granted
+        }
+
+        statusPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && statusPermission;
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // No explanation needed; request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_ACCESS_COARSE_RETURN_VALUE);
+
+        } else {
+            // Permission has already been granted
+        }
+
+        statusPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && statusPermission;
+
+        if(statusPermission){
+            startActivity(new Intent(PermissionsActivity.this, MapActivity.class));
+        } else{
+            Toast.makeText(this, "Missing Location Permission", Toast.LENGTH_LONG).show();
+        }
+
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_ACCESS_COARSE_RETURN_VALUE:
+            case MY_ACCESS_FINE_RETURN_VALUE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(new Intent(PermissionsActivity.this, MapActivity.class));
+                } else {
+                    Toast.makeText(this, "Missing Location Permission", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+
 }
